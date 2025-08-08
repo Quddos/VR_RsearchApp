722c5b28c2f15024b09643746bb6fdafb6d852d5
Raheem Quddus <84246065+Quddos@users.noreply.github.com>
2025-08-08 14:40:02 +0530

    implement time coutine
    Using IEnumerator to initiate a timestamp upon task assign to users

--- Assets/Scripts/ChemistryLabPT.cs
+++ Assets/Scripts/ChemistryLabPT.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.UIElements;
+using UnityEngine.TextCore.Text;
 
 public class ChemistryLabPT : MonoBehaviour
 {
-    public Button btn;
+    public UnityEngine.UI.Button btn;
     public GameObject PTButton;
     public GameObject Beaker;
     public GameObject Bottle;
     public TextMeshProUGUI robotInstructions;
+    public TextMeshProUGUI PTInstructions;
+    // public TextMeshProUGUI robotInformationColor;
 
     void Start()
     {
@@ -28,6 +32,18 @@ public class ChemistryLabPT : MonoBehaviour
         Bottle.SetActive(true);
         robotInstructions.text = "Grab the bottle and pour the liquid in it inot the Beaker";
         robotInstructions.color = Color.white;
+
+        StartCoroutine(StartTime());
+    }
+
+    IEnumerator StartTime()
+    {
+        yield return new WaitForSeconds(10.0f);
+        Beaker.SetActive(false);
+        Bottle.SetActive(false);
+        PTInstructions.text = "Practice completed, you will now complete a callibration process before conducting the study.";
+        robotInstructions.text = "Well done for completing this practice trial.";
+        // robotInformationColor.color =Color.green;
     }
 
     // Update is called once per frame
