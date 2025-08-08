2f9c708730f2c6d440ea871e985776a0cdbd14d4
Raheem Quddus <84246065+Quddos@users.noreply.github.com>
2025-08-08 10:57:20 +0530

    integrating model on screen display on Gameobject event

--- Assets/Scripts/ChemistryLabPT.cs
+++ Assets/Scripts/ChemistryLabPT.cs
@@ -2,20 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ChemistryLabPT : MonoBehaviour
 {
     public Button btn;
     public GameObject PTButton;
+    public GameObject Beaker;
+    public GameObject Bottle;
+    public TextMeshProUGUI robotInstructions;
 
     void Start()
     {
+        // by default the bottle will not appear
+        Beaker.SetActive(false);
+        Bottle.SetActive(false);
         btn.onClick.AddListener(PTButtonOnClick);
     }
 
     void PTButtonOnClick()
     {
+        // upon clicking the PracticalTrial Button the bottle and beaker will show to the user
         PTButton.SetActive(false);
+        Beaker.SetActive(true);
+        Bottle.SetActive(true);
+        robotInstructions.text = "Grab the bottle and pour the liquid in it inot the Beaker";
+        robotInstructions.color = Color.white;
     }
 
     // Update is called once per frame
